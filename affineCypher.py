e8587e9ac2538423fc402ef0bd89bfa43e030312
skmartinelli <skm010@ucsd.edu>
2020-03-16 04:18:17 -0700

    added decryption support

--- affineCypher.py
+++ affineCypher.py
@@ -25,6 +25,39 @@ if (encordec=="e"):
 		
 		print(chr(outputCharposition+97))
 
-		#print(outputCharposition)
+if (encordec=="d"):
+	for element in range(0, len(string)):
+		letter = string[element]
+		position = ord(letter)-97
+		
+		if (a==19):
+			mult=11
 
+		elif (a==9):
+			mult=3
+		elif (a==21):
+			mult=5
+		elif (a==15):
+			mult=7
+		elif (a==23):
+			mult=17
+		elif (a==3):
+			mult=9
+		elif (a==5):
+			mult=27
+		elif (a==7):
+			mult=15
+		elif (a==11):
+			mult=19
+		elif (a==17):
+			mult=23
+		elif (a==25):
+			mult=25
+		else:
+			print("nice job, a doesn't have an inverse")
+
+		outputCharposition= int((mult*(position-b))%26)
+		print(chr(outputCharposition+97))
+
+		
 
